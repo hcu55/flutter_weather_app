2c0b873cece435122139a8e83f5a9a6772a170ac
Chan Ui <hcu55@naver.com>
2023-03-25 21:27:40 +0900

    weather app

---
+++ lib/screens/loading.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Loading extends StatefulWidget {
+  const Loading({Key? key}) : super(key: key);
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.blue,
+            onPrimary: Colors.white
+          ),
+          child: const Text('Get my location'),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
