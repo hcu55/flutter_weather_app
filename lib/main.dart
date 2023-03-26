db0a6e39779cc39215270e61edb7f7b660a097e0
Chan Ui <hcu55@naver.com>
2023-03-26 16:43:35 +0900

    json test

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:weather_app/screens/loading.dart';
 
 void main(){
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -8,17 +11,57 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocation();
+    fetchData();
+  }
+
+  void getLocation() async{
+
+    try {
+      // LocationPermission permission = await Geolocator.requestPermission();
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      print(position);
+    }
+    catch(e){
+      print('인터넷 문제가 발생하였습니다.');
+    }
+  }
+  
+  void fetchData() async{
+    http.Response response = await http.get(Uri.parse('https://samples.openweathermap.org/data/2.5/weather?'
+        'q=London&appid=b1b15e88fa797225412429c1c50c122a1'));
+    if(response.statusCode == 200){
+      String jsonData = response.body;
+      var myJson = jsonDecode(jsonData)['weather'][0]['description'];
+      print(myJson);
+
+      var wind = jsonDecode(jsonData)['wind']['speed'];
+      print(wind);
+
+      var id = jsonDecode(jsonData)['id'];
+      print(id);
+    }
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
           style: ElevatedButton.styleFrom(
-            primary: Colors.blue,
-            onPrimary: Colors.white
+              backgroundColor: Colors.blue,
+              foregroundColor: Colors.white
           ),
+          onPressed:(){
+
+          },
           child: const Text('Get my location'),
-          onPressed: () {},
         ),
       ),
     );
